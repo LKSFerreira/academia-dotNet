50ca7e9e909c493109a917f33bca4692d4d49a24
LKSFerreira <lks_ferreira@hotmail.com>
2023-05-02 16:13:59 -0300

    :sparkles: Exercícios 01 e 02

--- orientacao-a-objeto/aula-01/poo-01/Program.cs
+++ orientacao-a-objeto/aula-01/poo-01/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Criação de um objeto da classe Carro");
+Console.WriteLine();
+
+Carro carro1 = new Carro("Fiat", "Uno", 2010);
+
+Console.WriteLine($"Carro 1 - Marca: {carro1.Marca}, Modelo: {carro1.Modelo}, Ano de Fabricação: {carro1.AnoFabricacao}");
--- orientacao-a-objeto/aula-01/poo-02/Program.cs
+++ orientacao-a-objeto/aula-01/poo-02/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Computador computador01 = new Computador("Dell", "Inspiron 15", "Notebook", 3500.00);
+Console.WriteLine(computador01.ToString());
+
+Computador computador02 = new Computador("HP", "Pavilion", "Notebook", 4500.00);
+Console.WriteLine(computador02.ToString());
