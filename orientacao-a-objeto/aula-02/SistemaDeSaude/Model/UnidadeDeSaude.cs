7b0e77ea681874eca39298ad88dc32ba99dca236
LKSFerreira <lks_ferreira@hotmail.com>
2023-05-22 14:59:29 -0300

    :tada: Iniciado Desafio de CRUD com plain text
    - A proposta é resolver os 2 desafios tanto em console quanto em Windows Form

--- orientacao-a-objeto/aula-02/SistemaDeSaude/Model/UnidadeDeSaude.cs
+++ orientacao-a-objeto/aula-02/SistemaDeSaude/Model/UnidadeDeSaude.cs
@@ -3,12 +3,12 @@ namespace SistemaDeSaude.Model
     public class UnidadeDeSaude
     {
 
-        public string? Nome { get; set; }
-        public string? Cnpj { get; set; }
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
         public int Telefone { get; set; }
-        public Endereco? Endereco { get; set; }
-        public Funcionario? Funcionario { get; set; }
-        public List<Paciente>? Pacientes { get; set; }
+        public Endereco Endereco { get; set; }
+        public Funcionario Funcionario { get; set; }
+        public List<Paciente> Pacientes { get; set; }
     }
 
 
